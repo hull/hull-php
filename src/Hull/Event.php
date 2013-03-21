9978094271368d73c819d3f85a5fbc24d64ee451
Xavier Cambar <xcambar@gmail.com>
2013-03-21 22:21:53 +0100

    Fixes Hull_Event

--- src/Hull/Event.php
+++ src/Hull/Event.php
@@ -8,16 +8,26 @@ class Hull_Event {
   public $timestamp;
   public $error;
 
-  function Hull_Event($secret) {
+  function Hull_Event($stream, $appSecret, $appId='') {
     try {
-      $appId      = $_SERVER['HULL_APP_ID'];
-      $sig        = explode('.', $_SERVER['HULL_SIGNATURE']);
+      $this->payload    = json_decode($stream);
+      $appId            = strlen($appId) ? $appId : $_SERVER['HTTP_HULL_APP_ID'];
+      if ($appId && $this->payload->appId && $appId !== $this->payload->appId) {
+        throw new Exception('inconsistent app IDs');
+      } elseif (!$appId) {
+        $appId = $this->payload->appId;
+      }
+
+      $signature        = $_SERVER['HTTP_HULL_SIGNATURE'];
+      $sig              = explode('.', $signature);
+      if (!$sig) {
+        throw new Exception ('Message not signed');
+      }
       $this->timestamp  = $sig[0];
       $this->nonce      = $sig[1];
       $hash             = $sig[2];
-      $this->payload    = json_decode(file_get_contents("php://input"));
-      $data = implode("-", array($this->timestamp, $this->nonce, $this->payload));
-      $this->valid      = $signature == hash_hmac("sha1", $data, $secret)
+      $data             = implode("-", array($this->timestamp, $this->nonce, $stream));
+      $this->valid      = $signature === hash_hmac("sha1", $data, $appSecret);
     } catch (Exception $e) {
       $this->error      = $e;
       $this->valid      = false;
