884e622e4b812884051aa5a24ba821e48f1ec1ea
Jimmy Oliger <hi@jimmyoliger.com>
2013-07-19 13:01:42 -0600

    Check if hull_ORGANIZATION_ID cookie exist.

--- src/Hull/Client.php
+++ src/Hull/Client.php
@@ -5,12 +5,12 @@ require_once 'Cache.php';
 require_once 'Event.php';
 
 class Hull_Client {
-    
+
   static $version = "0.1.0";
   static $dateFormat = "Y-m-d H:i:s O";
 
   public $debug = false;
-  
+
   static $configKeys = array('host', 'appId', 'appSecret');
 
   static $defaultConfig = array('host' => 'api.hullapp.io', 'debug' => false);
@@ -20,7 +20,7 @@ class Hull_Client {
     $config = self::parseConfig($o_config['hull']);
 
     $this->config = $config;
-    
+
     $this->host        = $config['host'];
     $this->appId       = $config['appId'];
     $this->appSecret   = $config['appSecret'];
@@ -55,9 +55,9 @@ class Hull_Client {
     $camel=preg_replace('/(?!^)[[:upper:]][[:lower:]]/', '$0', preg_replace('/(?!^)[[:upper:]]+/', $splitter.'$0', $camel));
     return strtolower($camel);
   }
-    
+
   private static function parseConfig($config=array()) {
-    
+
     foreach (self::$configKeys as $key) {
       $val = NULL;
       $envKey = "HULL_" . strtoupper(self::decamelize($key));
@@ -76,7 +76,9 @@ class Hull_Client {
   }
 
   public function currentUserId() {
-    $rawCookie = $_COOKIE['hull_' . $this->appId];
+    $cookieName = 'hull_' . $this->appId;
+    $rawCookie = isset($_COOKIE[$cookieName]) ? $_COOKIE[$cookieName] : false;
+
     if (!$rawCookie) {
       return;
     }
@@ -99,9 +101,9 @@ class Hull_Client {
       $url = '//' . $url;
     }
     //Assets have their own subdomain
-    return str_replace('//', '//assets.', $url); 
+    return str_replace('//', '//assets.', $url);
   }
-  
+
   public function userHash($userInfos) {
     if (!is_array($userInfos) || !isset($userInfos['email'])) {
       return false;
@@ -117,21 +119,21 @@ class Hull_Client {
   }
 
   // HTTP Plumbing...
-  
+
   public function get($path, $params=array(), $options=array()) {
     return $this->_exec("GET", $path, $params, $options);
   }
-  
+
   public function post($path, $data=array(), $options=array()) {
     return $this->_exec("POST", $path, $data, $options);
   }
-  
+
   public function put($path, $data=array(), $options=array()) {
     return $this->_exec("PUT", $path, $data, $options);
   }
 
   public function delete($path, $data=array(), $options=array()) {
-    return $this->_exec("DELETE", $path, $data, $options); 
+    return $this->_exec("DELETE", $path, $data, $options);
   }
 
   private function _exec($method, $path, $params=array(), $options=array()) {
@@ -140,7 +142,7 @@ class Hull_Client {
     } else {
       $headers = array();
     }
-    
+
     $res = $this->connection->exec(strtoupper($method), $path, $params, $headers);
 
     if (isset($options['raw'])) {
@@ -149,6 +151,4 @@ class Hull_Client {
       return $res['body'];
     }
   }
-  
-
 }
